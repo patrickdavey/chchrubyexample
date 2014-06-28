6a67531deae4e23b9c199ed4b48995b190b2c05d
Patrick Davey <patrick.davey@gmail.com>
2014-06-29 09:37:09 +1200

    Base 3.2.18 install with rspec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chchrubyexample::Application.config.secret_token = 'cdfe71a4e7d0f7f4632bea3cfd8a717f5973cf4e712ef11e68e534d0d21b499002b58fd2468f4b3edf7b462a47822692a84f307e000a233fe0778c7cb24dec0c'
